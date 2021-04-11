46c9cef853446d6b13ca1f9f18c9daa2a4bfc88e
antoroesler <anton.roesler98@gmx.de>
2021-04-11 09:11:15 +0200

    Added some new Simulations

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ from random import random, randint, choice
 import math
 import numpy as np
 
+NUMBER_OF_ANTS = 100
+TOTAL_FRAMES = 1000
+FPS = 30
+TRAILS = True
+
+
+
+TRAIL_FREQUENCY = 4
+
 class Simulator:
     def __init__(self):
         self.ants = []
@@ -12,22 +21,23 @@ class Simulator:
         self.home = [0, 0]
         self.home_trail= []
         self.food_trail= [[9, 7]]
+        self.iteration = -1
     
     def create_new_ant(self, n=1):
         for _ in range(n):
             self.ants.append(Ant())
     
     def step(self):
+        self.iteration += 1
         """
-        if len(self.home_trail) > 30000:
-            self.home_trail = self.home_trail[-30000:]
-        if len(self.food_trail) > 30000:
-            self.food_trail = self.food_trail[-30000:]
+        if len(self.home_trail) > 50000:
+            self.home_trail = self.home_trail[-50000:]
+        if len(self.food_trail) > 50000:
+            self.food_trail = self.food_trail[-50000:]
         """
-        for ant in self.ants:
-            ant.forward()
        
         for ant in self.ants:
+            ant.forward()
             if ant.pos[0] > 20 or ant.pos[0] < -10 or ant.pos[1] > 20 or ant.pos[1] < -10:
                 self.ants.remove(ant) # Kill ants that are to far away
                 continue
@@ -35,7 +45,7 @@ class Simulator:
                 if ant.reach_object(self.food):
                     ant.has_food = True
                     ant.angle = (ant.angle + 120) % 360 # turn around 120°
-                elif ant.in_sight(self.food, turn=True, strong=1.5): 
+                elif ant.in_sight(self.food, turn=True, strong=1.5) and self.iteration%TRAIL_FREQUENCY == 0: 
                     self.home_trail.append(ant.pos)
                     
                 else:
@@ -43,17 +53,19 @@ class Simulator:
                         ant.forward() 
                     else:
                         ant.angle+=randint(-20,20)
-                    self.home_trail.append(ant.pos)
+                    if self.iteration%TRAIL_FREQUENCY == 0:
+                        self.home_trail.append(ant.pos)
                     
             else:
                 if ant.reach_object(self.home):
                     ant.has_food = False
                     ant.angle = (ant.angle + 120) % 360 # turn around 120°
-                elif ant.in_sight(self.home, turn=True):
+                elif ant.in_sight(self.home, turn=True) and self.iteration%TRAIL_FREQUENCY == 0:
                     self.food_trail.append(ant.pos)
                 else:
                     ant.in_sight(self.home_trail, turn=True)
-                    self.food_trail.append(ant.pos)
+                    if self.iteration%TRAIL_FREQUENCY == 0:
+                        self.food_trail.append(ant.pos)
     
     def get_ant_pos(self):
         xs = []
@@ -116,25 +128,27 @@ class Ant:
         return Rot.dot(vector)
 
 s = Simulator()
-s.create_new_ant(n=100)
-
+num_ants = 0
 plt.scatter([],[])
 
 def animate(some):
+    if len(s.ants) < NUMBER_OF_ANTS:
+        s.create_new_ant()
     plt.cla()
     s.step()
+    if TRAILS:
+        htx, hty = np.transpose(np.array(s.home_trail))
+        plt.scatter(htx, hty, alpha=0.1, s=4, c="r")
+        ftx, fty = np.transpose(np.array(s.food_trail))
+        plt.scatter(ftx, fty, alpha=0.1, s=4, c="y")
     x,y = s.get_ant_pos()
     plt.xlim([-1, 10])
     plt.ylim([-1, 10])
-    plt.scatter(x, y)
+    plt.scatter(x, y, s=10)
     fx, fy = s.food[0]
     plt.scatter(fx, fy, s=100)
     plt.scatter(0, 0, s=100)
-    htx, hty = np.transpose(np.array(s.home_trail))
-    plt.scatter(htx, hty, alpha=0.05, s=4, c="r")
-    ftx, fty = np.transpose(np.array(s.food_trail))
-    plt.scatter(ftx, fty, alpha=0.1, s=4, c="y")
 
-anim = FuncAnimation(plt.gcf(), animate, frames=1000)
-writer = PillowWriter(fps=50) 
-anim.save("ant_simulation.gif", writer=writer)
+anim = FuncAnimation(plt.gcf(), animate, frames=TOTAL_FRAMES)
+writer = PillowWriter(fps=FPS) 
+anim.save("ant_simulation8.gif", writer=writer)
